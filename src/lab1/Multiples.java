389cc9b135ef4c4e33b71df90146dd2a368f4af1
JamesPapadakos <james.papadakos@mail.utoronto.ca>
2025-09-09 17:44:14 -0400

    bug fix

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,22 +2,23 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        //System.out.println(multiples(int, int, int));
+        //System.out.println(multiples());
+
     }
 
-     static int multiples(int n, int a, int b) {
-        int j = 0;
-        for (int i = 1; i < n; i++) {
-            boolean divisibleBya = i % a == 0;
-            boolean divisibleByb = i % b == 0;
+         public static int multiples(int n, int a, int b) {
+            int j = 0;
+            for (int i = 1; i < n; i++) {
+                boolean divisibleBya = i % a == 0;
+                boolean divisibleByb = i % b == 0;
 
-            if (divisibleBya) {
-                j++;
+                if (divisibleBya) {
+                    j++;
+                }
+                else if (divisibleByb) {
+                    j++;
+                }
             }
-            else if (divisibleByb) {
-                j++;
-            }
-        }
-        return j;
+            return j;
     }
 }

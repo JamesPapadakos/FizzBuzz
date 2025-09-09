e840b50f59af811539e74a9c8b42e3351e62e0d6
JamesPapadakos <james.papadakos@mail.utoronto.ca>
2025-09-09 16:39:59 -0400

    Changed the syntax

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -15,7 +15,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -40,5 +40,8 @@ class FizzBuzz {
         }
         i++;
         return i;
+    }
+
+    public static void main() {
     }
 }

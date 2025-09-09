e0134ea7de5d4aef5aed1e2bf4aeb73eea634ba8
JamesPapadakos <james.papadakos@mail.utoronto.ca>
2025-09-09 17:51:44 -0400

    added the overloading

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,6 +5,9 @@ public class Multiples {
         //System.out.println(multiples());
 
     }
+        public static int multiples() {
+            return multiples(1000, 3, 5);
+        }
 
          public static int multiples(int n, int a, int b) {
             int j = 0;
